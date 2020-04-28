1065c871ff053f56c2bf010f9bf73b9ec194caa6
Bill Kidwell <bill.kidwell@bigassfans.com>
2020-04-28 11:36:54 -0400

    Provision device and cert

---
+++ provision/createCert.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Generating certs for device: "$1
+
+COMMON_NAME=$1
+# Device Dir
+DDIR=./devices/${COMMON_NAME}
+CADIR=../certs
+
+# Generate a private key
+openssl genrsa -out ${DDIR}/deviceCert.key 2048
+
+# Generate the csr
+openssl req -new -config ${DDIR}/csr.cnf \
+    -key ${DDIR}/deviceCert.key \
+    -out ${DDIR}/deviceCert.csr
+
+# Create the x509 certificate
+openssl x509 -req -in ${DDIR}/deviceCert.csr \
+    -CA ${CADIR}/sampleCACertificate.pem \
+    -CAkey ${CADIR}/sampleCACertificate.key \
+    -CAcreateserial \
+    -out ${DDIR}/deviceCert.crt \
+    -days 365 -sha256
+
+# Create a certificate file
+cat ${DDIR}/deviceCert.crt \
+    ${CADIR}/sampleCACertificate.pem > ${DDIR}/deviceCertAndCACert.crt
+
+# Create the certificate package
+cp ${CADIR}/root.cert ${DDIR}/root.cert
+cd ${DDIR}
+tar czvf certs.tar.gz deviceCert.crt \
+    deviceCertAndCACert.crt \
+    root.cert
+
+rm root.cert
